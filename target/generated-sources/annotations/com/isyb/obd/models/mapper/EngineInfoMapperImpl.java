3e4f09acfd78423b2164456708543c0fd365ae71
Vadim Kain <kain.vadim50@gmail.com>
2024-02-08 23:21:00 +0200

    IOBD-125 Created Dockerfile

--- target/generated-sources/annotations/com/isyb/obd/models/mapper/EngineInfoMapperImpl.java
+++ target/generated-sources/annotations/com/isyb/obd/models/mapper/EngineInfoMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-02-08T16:45:16+0200",
+    date = "2024-02-08T20:15:54+0200",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.10 (Amazon.com Inc.)"
 )
 @Component
